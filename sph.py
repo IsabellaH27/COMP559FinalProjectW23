89b31f2cc939cb936845ce4c5f0c60474d933bab
Isabella Hao <isabellahao370@gmail.com>
2023-04-21 23:06:50 -0400

    Update sph.py

--- sph.py
+++ sph.py
@@ -1,199 +1,57 @@
-"""
-Simulates the free-surface flow of "pouring a beer" using Smoothed Particle
-Hydrodynamics (SPH). Uses the super-simple approch of Matthias Müller:
-https://matthias-research.github.io/pages/publications/sca03.pdf
-
-Simulates the Navier-Stokes Momentum in Lagrangian Form
-
-    ρ Du/Dt = − ∇p + μ ∇²u + g
-
-u : velocity
-ρ : density
-p : pressure
-μ : dynamics viscosity
-g : gravity
-
-Du/Dt : Lagrangian temporal derivative (=Material Derivative)
-∇p    : Pressure Gradient
-∇²u   : Velocity Laplacian (=collection of second derivatives)
-
-IMPORTANT: The simulated fluid is not incompressible.
-
-----------
-
-Solution Strategy:
-
-Discretize the fluid by N particles (i=0, 1, ..., N-1) that smooth
-their properties radially with some smoothing kernels. Then the Momentum
-Equation discretizes to
-
-Duᵢ/Dt = Pᵢ + Vᵢ + G
-
-with
-
-uᵢ : The velocity of the i-th smoothed particle
-Pᵢ : The pressure forces acting on the i-th smoothed particle
-Vᵢ : The viscosity forces acting on the i-th smoothed particle
-G  : The gravity forces (acting equally on all particles)
-
-This yields a set of N ODEs each for the two velocity components (in case
-of 2D) of the particles. These can be solved using a (simpletic) integrator
-to advance the position of the particles.
-
-
-Let xᵢ be the 2D position of each smoothed particle.
-
-Let L be the smoothing length of each smoothed particle.
-
-Let M be the mass of each smoothed particle.
-
-------
-
-Algorithm
-
-(for details on the chosen smoothing kernels, see the paper mentioned above)
-
-1. Compute the rhs for each particle Fᵢ
-
-    1.1 Compute the distances between all particle positions
-
-        dᵢⱼ = || xᵢ − xⱼ ||₂
-
-    1.2 Compute the density at each particle's position
-
-        ρᵢ = (315 M) / (64 π L⁹) ∑ⱼ (L² − dᵢⱼ²)³
-    
-    1.3 Compute the pressure at each particle's position (κ is the isentropic
-        exponent, ρ₀ is a base density)
-
-        pᵢ = κ * (ρ − ρ₀)
-
-    1.4 Compute the pressure force of each particle
-
-        Pᵢ = (− (45 M) / (π L⁶)) ∑ⱼ − (xⱼ − xᵢ) / (dᵢⱼ) (pⱼ + pᵢ) / (2 ρⱼ) (L − dᵢⱼ)²
-    
-    1.5 Compute the viscosity force of each particle
-
-        Vᵢ = (45 μ M) / (π L⁶) ∑ⱼ (uⱼ − uᵢ) / (ρⱼ) (L − dᵢⱼ)
-    
-    1.6 Add up the RHS
-
-        Fᵢ = Pᵢ + Vᵢ + G
-
-2. Integrate the Ordinary Differential Equation  "ρ Duᵢ/Dt = Fᵢ" with a
-   Δt timestep
-
-    2.1 Update the particles' velocities
-
-        uᵢ ← uᵢ + Δt Fᵢ / ρᵢ
-    
-    2.2 Update the particles' positions
-
-        xᵢ ← xᵢ + Δt uᵢ
-
-3. Enforce the wall Boundary Conditions. If a particle leaves the
-   domain then:
-
-    3.1 Set its position to the Boundary
-
-    3.2 Inverse its velocity component perpendicular to the wall
-
-    3.3 Multiply the velocity component perpendicular to the
-        wall with a damping factor
-
--------
-
-Computational Considerations.
-
-1. The steps on computing density, pressure force and viscosity force
-   involve the computation of the various smoothing kernels. Those
-   always consist of a constant part that is due to the normalization
-   which can be precomputed.
-
-2. When applying summations in the distance calculations and when
-   applying the smothing kernels in density, pressure force and
-   viscosity force calculations only OTHER PARTICLES IN THE
-   SMOOTHING LENGTH OF THE CONSIDERED PARTICLE ARE RELEVANT. Hence,
-   we can use efficient neighbor computation routines.
-
--------
-
-Take care that the ODE integration can become instable when using too
-large time steps.
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import neighbors
 from tqdm import tqdm
 
-MAX_PARTICLES = 50
-DOMAIN_WIDTH = 20
-DOMAIN_HEIGHT = 80
-
-PARTICLE_MASS = 1
-ISOTROPIC_EXPONENT = 20
-BASE_DENSITY = 1
-SMOOTHING_LENGTH = 5
-DYNAMIC_VISCOSITY = 0.5
-DAMPING_COEFFICIENT = - 0.9
+BASE_DENSITY = 2
 CONSTANT_FORCE = np.array([[0.0, -0.1]])
-
+CONTAINERW = 60
+CONTAINERH = 80
+DYNAMIC_VISCOSITY = 0.5
+DAMPING_COEFF = - 0.9
+MAX_PARTICLES = 1000
+P_MASS = 2
+ISOTROPIC_EXPONENT = 30
+SMOOTHING_LEN = 3
 TIME_STEP_LENGTH = 0.01
-N_TIME_STEPS = 2_500
-ADD_PARTICLES_EVERY = 5
-
+NUM_TIMESTEPS = 2500
+INSERT_GAP = 1
 FIGURE_SIZE = (2, 3)
 PLOT_EVERY = 1
-SCATTER_DOT_SIZE = 1
+SCATTER_DOT_SIZE = 2
 
-DOMAIN_X_LIM = np.array([
-    SMOOTHING_LENGTH,
-    DOMAIN_WIDTH - SMOOTHING_LENGTH,
+CONTAINER_LIMIT_X = np.array([
+    SMOOTHING_LEN,CONTAINERW - SMOOTHING_LEN,
 ])
-DOMAIN_Y_LIM = np.array([
-    SMOOTHING_LENGTH,
-    DOMAIN_HEIGHT - SMOOTHING_LENGTH,
+CONTAINER_LIMIT_Y = np.array([
+    SMOOTHING_LEN, CONTAINERH - SMOOTHING_LEN,
 ])
 
 NORMALIZATION_DENSITY = (
-    (
-        315 * PARTICLE_MASS
-    ) / (
-        64 * np.pi * SMOOTHING_LENGTH**9
-    )
+    (315 * P_MASS) / (64 * np.pi * pow(SMOOTHING_LEN,9))
 )
 NORMALIZATION_PRESSURE_FORCE = (
-    -
-    (
-        45 * PARTICLE_MASS
-    ) / (
-        np.pi * SMOOTHING_LENGTH**6
-    )
+    -(45 * P_MASS) / (np.pi * pow(SMOOTHING_LEN,6))
 )
 NORMALIZATION_VISCOUS_FORCE = (
-    (
-        45 * DYNAMIC_VISCOSITY * PARTICLE_MASS
-    ) / (
-        np.pi * SMOOTHING_LENGTH**6
-    )
+    (45 * DYNAMIC_VISCOSITY * P_MASS) / (np.pi * pow(SMOOTHING_LEN,6))
 )
 
 def main():
     n_particles = 1
 
-    positions = np.zeros((n_particles, 2))
-    velocities = np.zeros_like(positions)
-    forces = np.zeros_like(positions)
+    pos = np.zeros((n_particles, 2))
+    vel = np.zeros_like(pos)
+    forces = np.zeros_like(pos)
 
-    plt.style.use("dark_background")
     plt.figure(figsize=FIGURE_SIZE, dpi=160)
 
-    for iter in tqdm(range(N_TIME_STEPS)):
-        if iter % ADD_PARTICLES_EVERY == 0 and n_particles < MAX_PARTICLES:
+    for iter in tqdm(range(NUM_TIMESTEPS)):
+        if iter % INSERT_GAP == 0 and n_particles < MAX_PARTICLES:
             new_positions = np.array([
-                [10 + np.random.rand(), DOMAIN_Y_LIM[1]],
-                [15 + np.random.rand(), DOMAIN_Y_LIM[1]],
-                [20 + np.random.rand(), DOMAIN_Y_LIM[1]],
+                [10 + np.random.rand(), CONTAINER_LIMIT_Y[1]],
+                [15 + np.random.rand(), CONTAINER_LIMIT_Y[1]],
+                [20 + np.random.rand(), CONTAINER_LIMIT_Y[1]],
             ])
             
             new_velocities = np.array([
@@ -204,116 +62,77 @@ def main():
 
             n_particles += 3
 
-            positions = np.concatenate((positions, new_positions), axis=0)
-            velocities = np.concatenate((velocities, new_velocities), axis=0)
+            pos = np.concatenate((pos, new_positions), axis=0)
+            vel = np.concatenate((vel, new_velocities), axis=0)
         
-        neighbor_ids, distances = neighbors.KDTree(
-            positions,
+        neighbor_ids, dist = neighbors.KDTree(
+            pos,
         ).query_radius(
-            positions,
-            SMOOTHING_LENGTH,
+            pos,
+            SMOOTHING_LEN,
             return_distance=True,
             sort_results=True,
         )
 
-        densities = np.zeros(n_particles)
+        dens = np.zeros(n_particles)
 
         for i in range(n_particles):
-            for j_in_list, j in enumerate(neighbor_ids[i]):
-                densities[i] += NORMALIZATION_DENSITY * (
-                    SMOOTHING_LENGTH**2
-                    -
-                    distances[i][j_in_list]**2
-                )**3
+            for k, j in enumerate(neighbor_ids[i]):
+                dens[i] += NORMALIZATION_DENSITY * pow((pow(SMOOTHING_LEN,2)-pow(dist[i][k],2 )),3)
         
-        pressures = ISOTROPIC_EXPONENT * (densities - BASE_DENSITY)
+        pressures = ISOTROPIC_EXPONENT * (dens - BASE_DENSITY)
 
-        forces = np.zeros_like(positions)
+        forces = np.zeros_like(pos)
 
         # Drop the element itself
         neighbor_ids = [ np.delete(x, 0) for x in neighbor_ids]
-        distances = [ np.delete(x, 0) for x in distances]
+        dist = [ np.delete(x, 0) for x in dist]
 
         for i in range(n_particles):
-            for j_in_list, j in enumerate(neighbor_ids[i]):
+            for k, j in enumerate(neighbor_ids[i]):
                 # Pressure force
-                forces[i] += NORMALIZATION_PRESSURE_FORCE * (
-                    -
-                    (
-                        positions[j]
-                        -
-                        positions[i]
-                    ) / distances[i][j_in_list]
-                    *
-                    (
-                        pressures[j]
-                        +
-                        pressures[i]
-                    ) / (2 * densities[j])
-                    *
-                    (
-                        SMOOTHING_LENGTH
-                        -
-                        distances[i][j_in_list]
-                    )**2
-                )
-
+                forces[i] += NORMALIZATION_PRESSURE_FORCE*(-(pos[j]-pos[i]) / dist[i][k]*(pressures[j]+pressures[i]) 
+                    / (2*dens[j])*pow((SMOOTHING_LEN-dist[i][k]),2))
                 # Viscous force
                 forces[i] += NORMALIZATION_VISCOUS_FORCE * (
-                    (
-                        velocities[j]
-                        -
-                        velocities[i]
-                    ) / densities[j]
-                    *
-                    (
-                        SMOOTHING_LENGTH
-                        -
-                        distances[i][j_in_list]
-                    )
+                    (vel[j]-vel[i]) / dens[j]*(SMOOTHING_LEN-dist[i][k])
                 )
         
         # Force due to gravity
-        forces += CONSTANT_FORCE
-
-        ### There is an error in the video, in that the gravity is divided by the density which
-        ### wrongly scales it. Uncommenting the below, and commenting the call above will fix
-        ### that but, also slightly changes the visual result of the simulation.
-        # forces += CONSTANT_FORCE * densities[:, np.newaxis]
+        forces += CONSTANT_FORCE * dens[:, np.newaxis]
         
 
         # Euler Step
-        velocities = velocities + TIME_STEP_LENGTH * forces / densities[:, np.newaxis]
-        positions = positions + TIME_STEP_LENGTH * velocities
+        vel = vel + TIME_STEP_LENGTH * forces / dens[:, np.newaxis]
+        pos = pos + TIME_STEP_LENGTH * vel
 
         # Enfore Boundary Conditions
-        out_of_left_boundary = positions[:, 0] < DOMAIN_X_LIM[0]
-        out_of_right_boundary = positions[:, 0] > DOMAIN_X_LIM[1]
-        out_of_bottom_boundary = positions[:, 1] < DOMAIN_Y_LIM[0]
-        out_of_top_boundary = positions[:, 1] > DOMAIN_Y_LIM[1]
+        out_of_left_boundary = pos[:, 0] < CONTAINER_LIMIT_X[0]
+        out_of_right_boundary = pos[:, 0] > CONTAINER_LIMIT_X[1]
+        out_of_bottom_boundary = pos[:, 1] < CONTAINER_LIMIT_Y[0]
+        out_of_top_boundary = pos[:, 1] > CONTAINER_LIMIT_Y[1]
 
-        velocities[out_of_left_boundary, 0]     *= DAMPING_COEFFICIENT
-        positions [out_of_left_boundary, 0]      = DOMAIN_X_LIM[0]
+        vel[out_of_left_boundary, 0]     *= DAMPING_COEFF
+        pos [out_of_left_boundary, 0]      = CONTAINER_LIMIT_X[0]
 
-        velocities[out_of_right_boundary, 0]    *= DAMPING_COEFFICIENT
-        positions [out_of_right_boundary, 0]     = DOMAIN_X_LIM[1]
+        vel[out_of_right_boundary, 0]    *= DAMPING_COEFF
+        pos [out_of_right_boundary, 0]     = CONTAINER_LIMIT_X[1]
 
-        velocities[out_of_bottom_boundary, 1]   *= DAMPING_COEFFICIENT
-        positions [out_of_bottom_boundary, 1]    = DOMAIN_Y_LIM[0]
+        vel[out_of_bottom_boundary, 1]   *= DAMPING_COEFF
+        pos [out_of_bottom_boundary, 1]    = CONTAINER_LIMIT_Y[0]
 
-        velocities[out_of_top_boundary, 1]      *= DAMPING_COEFFICIENT
-        positions [out_of_top_boundary, 1]       = DOMAIN_Y_LIM[1]
+        vel[out_of_top_boundary, 1]      *= DAMPING_COEFF
+        pos [out_of_top_boundary, 1]       = CONTAINER_LIMIT_Y[1]
 
         if iter % PLOT_EVERY == 0:
             plt.scatter(
-                positions[:, 0],
-                positions[:, 1],
+                pos[:, 0],
+                pos[:, 1],
                 s=SCATTER_DOT_SIZE,
-                c=positions[:, 1],
-                cmap="Wistia_r",
+                c=pos[:, 1],
             )
-            plt.ylim(DOMAIN_Y_LIM)
-            plt.xlim(DOMAIN_X_LIM)
+            plt.ylim(CONTAINER_LIMIT_Y)
+            plt.xlim(CONTAINER_LIMIT_X)
             plt.xticks([], [])
             plt.yticks([], [])
             plt.tight_layout()
