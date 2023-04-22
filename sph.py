9f5bbb41c722645e9d7d9a633026b8f86eaa6af2
Isabella Hao <isabellahao370@gmail.com>
2023-04-22 00:12:36 -0400

    bugfix

--- sph.py
+++ sph.py
@@ -9,7 +9,7 @@ CONTAINERW = 60
 CONTAINERH = 80
 DYNAMIC_VISCOSITY = 0.5
 DAMPING_COEFF = - 0.9
-MAX_PARTICLES = 1000
+MAX_PARTICLES = 100
 P_MASS = 2
 ISOTROPIC_EXPONENT = 30
 SMOOTHING_LEN = 3
@@ -20,22 +20,11 @@ FIGURE_SIZE = (2, 3)
 PLOT_EVERY = 1
 PARTICLE_SIZE = 2
 
-CONTAINER_LIMIT_X = np.array([
-    SMOOTHING_LEN,CONTAINERW - SMOOTHING_LEN,
-])
-CONTAINER_LIMIT_Y = np.array([
-    SMOOTHING_LEN, CONTAINERH - SMOOTHING_LEN,
-])
-
-NORMALIZATION_DENSITY = (
-    (315 * P_MASS) / (64 * np.pi * pow(SMOOTHING_LEN,9))
-)
-NORMALIZATION_PRESSURE_FORCE = (
-    -(45 * P_MASS) / (np.pi * pow(SMOOTHING_LEN,6))
-)
-NORMALIZATION_VISCOUS_FORCE = (
-    (45 * DYNAMIC_VISCOSITY * P_MASS) / (np.pi * pow(SMOOTHING_LEN,6))
-)
+CONTAINER_LIMIT_X = np.array([SMOOTHING_LEN,CONTAINERW - SMOOTHING_LEN,])
+CONTAINER_LIMIT_Y = np.array([SMOOTHING_LEN, CONTAINERH - SMOOTHING_LEN,])
+NORMALIZATION_DENSITY = ((315 * P_MASS) / (64 * np.pi * pow(SMOOTHING_LEN,9)))
+NORMALIZATION_PRESSURE_FORCE = ( -(45 * P_MASS) / (np.pi * pow(SMOOTHING_LEN,6)))
+NORMALIZATION_VISCOUS_FORCE = ((45 * DYNAMIC_VISCOSITY * P_MASS) / (np.pi * pow(SMOOTHING_LEN,6)))
 
 def main():
     n_particles = 1
@@ -46,8 +35,8 @@ def main():
 
     plt.figure(figsize=FIGURE_SIZE, dpi=160)
 
-    for iter in tqdm(range(NUM_TIMESTEPS)):
-        if iter % INSERT_GAP == 0 and n_particles < MAX_PARTICLES:
+    for timesteps in tqdm(range(NUM_TIMESTEPS)):
+        if timesteps % INSERT_GAP == 0 and n_particles < MAX_PARTICLES:
             new_positions = np.array([
                 [10 + np.random.rand(), CONTAINER_LIMIT_Y[1]],
                 [15 + np.random.rand(), CONTAINER_LIMIT_Y[1]],
@@ -112,16 +101,17 @@ def main():
         out_bottom = pos[:, 1] < CONTAINER_LIMIT_Y[0]
         out_top = pos[:, 1] > CONTAINER_LIMIT_Y[1]
 
-        vel[out_left, 0]*= DAMPING_COEFF
-        pos [out_left, 0]= CONTAINER_LIMIT_X[0]
-        vel[out_right, 0]*= DAMPING_COEFF
-        pos [out_right, 0]= CONTAINER_LIMIT_X[1]
         vel[out_bottom, 1]*= DAMPING_COEFF
         pos [out_bottom, 1]= CONTAINER_LIMIT_Y[0]
         vel[out_top, 1]*= DAMPING_COEFF
         pos [out_top, 1] = CONTAINER_LIMIT_Y[1]
+        vel[out_left, 0]*= DAMPING_COEFF
+        pos [out_left, 0]= CONTAINER_LIMIT_X[0]
+        vel[out_right, 0]*= DAMPING_COEFF
+        pos [out_right, 0]= CONTAINER_LIMIT_X[1]
 
-        if iter % PLOT_EVERY == 0:
+
+        if timesteps % PLOT_EVERY == 0:
             plt.scatter(
                 pos[:, 0],
                 pos[:, 1],
